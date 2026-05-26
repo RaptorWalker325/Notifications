624a437a6f0f023360bc32d67668ad3660d8fd34
RaptorWalker325 <thiago.ortiz.2002@gmail.com>
2026-05-26 08:22:46 -0300

    reading a json file

--- Notifications.py
+++ Notifications.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-today = datetime.today().strftime("%d/%m/%Y")
+from Lib import json
+
+today = "22/05/2026" #datetime.today().strftime("%d/%m/%Y")
 today = datetime.strptime(today, "%d/%m/%Y")
 class Notification: 
     def __init__(self, name: str, date: str):
@@ -12,14 +14,12 @@ def add_notification(name, date):
     notification = Notification(name, date)
     notifications.append(notification)
     
-add_notification("Yesterday", "21/05/2026")
-add_notification("Today", "22/05/2026")
-add_notification("Tomorrow", "23/05/2026")
-add_notification("In 15 days", "06/06/2026")
-add_notification("In 30 days", "21/06/2026")
-add_notification("In 31 days", "22/06/2026")
+with open('data.json', 'r') as file:
+    data = json.load(file)
+for item in data:
+    add_notification(item['name'], item['date'])
 
 for i in range(len(notifications)):
-    print(i)
     calc = datetime.strptime(notifications[i].date, "%d/%m/%Y") - today
     if  calc.days <= 30 and calc.days >= 0:print(f"This notifications expires over the next 30 days:\n {notifications[i].name}\n {notifications[i].date}\n")
+
