d560b32bd87d297e6f8dfef4514e4aed9c120ac5
RaptorWalker325 <thiago.ortiz.2002@gmail.com>
2026-05-28 10:12:13 -0300

    added more modularity

--- Notifications.py
+++ Notifications.py
@@ -21,6 +21,7 @@ def input_notifications():
     name = input("Enter the name of the notification: ")
     date = input("Enter the date of the notification (dd/mm/yyyy): ")
     add_notification(name, date)
+    print("Notification added successfully.")
     
 def list_notifications():
     for notification in notifications:
@@ -30,52 +31,29 @@ def search_notification(name):
     for i in range(len(notifications)):
         if notifications[i].name == name:
             return i
-    return -1      
-def edit_notification(index, name, date):
-    notifications[index].name = name
-    notifications[index].date = date
-    
-def delete_notification(index):
-    del notifications[index]
-           
-#load from json file    
-with open('data.json', 'r') as file:
-    data = json.load(file)
-for item in data:
-    add_notification(item['name'], item['date'])
-    
-while menu != "0":
-    print("Notifications:\n 1 - Add a notification\n 2 - List notifications\n 3 - Edit a notification \n 4 - Delete a notification\n 0 - Exit")
-    menu = input("Enter your choice: ")
-    if menu == "1":
-        input_notifications()
-    elif menu == "2":
-        list_notifications()
-    elif menu == "3":
-        name = input("Enter the name of the notification to edit: ")
-        index = search_notification(name)
-        if index != -1:
+    return -1 
+     
+def edit_notification():
+    name = input("Enter the name of the notification to edit: ")
+    index = search_notification(name)
+    if index != -1:
             new_name = input("Enter the new name of the notification: ")
             new_date = input("Enter the new date of the notification (dd/mm/yyyy): ")
-            edit_notification(index, new_name, new_date)
-        else:
+            notifications[index].name = new_name
+            notifications[index].date = new_date
+    else:
             print("Notification not found.")
-    elif menu == "4":
-        name = input("Enter the name of the notification to delete: ")
-        index = search_notification(name)
-        if index != -1:
-            delete_notification(index)
-        else:
-            print("Notification not found.")        
-    elif menu == "0":
-        print("Exiting...")        
+    
+def delete_notification():
+    name = input("Enter the name of the notification to delete: ")
+    index = search_notification(name)
+    if index != -1:
+        del notifications[index]
+    else:
+        print("Notification not found.")  
 
-print("Notifications expiring in the next 30 days:")
-for i in range(len(notifications)):
-    calc = datetime.strptime(notifications[i].date, "%d/%m/%Y") - today
-    if  calc.days <= 30 and calc.days >= 0:
-        print(f"{notifications[i].name}\n{notifications[i].date}\n")
-        async def main():
+def windows_notification(i):
+    async def main():
             await notifier.send(
                 title="Expiring in 30 days",
                 message=f"{notifications[i].name} is expiring on {notifications[i].date}",
@@ -87,12 +65,50 @@ for i in range(len(notifications)):
                 ],
                 sound=DEFAULT_SOUND
             )
-        asyncio.run(main())
+    asyncio.run(main())        
 
-#write to json file
-with open('data.json', 'w') as file:
-    newdata = []
-    for notification in notifications:
-        newdata.append({"name": notification.name, "date": notification.date})
-    json_string = json.dumps(newdata, indent=4)
-    file.write(json_string)
+def console_notification(i):
+    print(f"{notifications[i].name} is expiring on {notifications[i].date}")
+           
+def check_expiring_notifications(expiring):
+    print(f"Notifications expiring in the next {expiring} days:")
+    for i in range(len(notifications)):
+        calc = datetime.strptime(notifications[i].date, "%d/%m/%Y") - today
+        if  calc.days <= expiring and calc.days >= 0:
+            #windows_notification(i)
+            console_notification(i)
+           
+def opendata(): 
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    for item in data:
+        add_notification(item['name'], item['date'])
+
+def savingdata():
+    with open('data.json', 'w') as file:
+        newdata = []
+        for notification in notifications:
+            newdata.append({"name": notification.name, "date": notification.date})
+        file.write(json.dumps(newdata, indent=4))    
+
+opendata()    
+while menu != "0":
+    print("Notifications:\n 1 - Add a notification\n 2 - List notifications\n 3 - Edit a notification \n 4 - Delete a notification\n 5 - Check expiring notifications\n 0 - Exit")
+    menu = input("Enter your choice: ")
+    if menu == "1":
+        input_notifications()
+    elif menu == "2":
+        list_notifications()
+    elif menu == "3":
+        edit_notification()        
+    elif menu == "4":
+        delete_notification()
+    elif menu == "5":
+        expiring = int(input("check notifications for expiring in the next: "))
+        check_expiring_notifications(expiring)
+    elif menu == "0":
+        print("Exiting...")        
+savingdata()
+        
+        
+
